aa6e60ef1dcb2ad0042b8f77518e21f6765fc3f4
aalexebr <alex.ebr@icloud.com>
2023-07-06 13:58:14 +0200

    ADD- strutcure and mileston 1 almost complete

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 Vue.createApp({
     data(){
         return{
+            indexCounter: null,
             contacts: [
                 {
                     name: 'Michele',
-                    avatar: './img/avatar_1.png',
+                    avatar: 'img/avatar_1.jpg',
                     visible: true,
                     messages: [
                             {
@@ -26,7 +27,7 @@ Vue.createApp({
                 },
                 {
                     name: 'Fabio',
-                    avatar: './img/avatar_2.png',
+                    avatar: 'img/avatar_2.jpg',
                     visible: true,
                     messages: [
                             {
@@ -48,7 +49,7 @@ Vue.createApp({
                 },
                 {
                     name: 'Samuele',
-                    avatar: './img/avatar_3.png',
+                    avatar: 'img/avatar_3.jpg',
                     visible: true,
                     messages: [
                         {
@@ -70,7 +71,7 @@ Vue.createApp({
                 },
                 {
                     name: 'Alessandro B.',
-                    avatar: './img/avatar_4.png',
+                    avatar: 'img/avatar_4.jpg',
                     visible: true,
                     messages: [
                         {
@@ -87,7 +88,7 @@ Vue.createApp({
                 },
                 {
                     name: 'Alessandro L.',
-                    avatar: './img/avatar_5.png',
+                    avatar: 'img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -104,7 +105,7 @@ Vue.createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.png',
+                    avatar: 'img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -126,7 +127,7 @@ Vue.createApp({
                 },
                 {
                     name: 'Federico',
-                    avatar: './img/avatar_7.png',
+                    avatar: 'img/avatar_7.jpg',
                     visible: true,
                     messages: [
                         {
@@ -143,7 +144,7 @@ Vue.createApp({
                 },
                 {
                     name: 'Davide',
-                    avatar: './img/avatar_8.png',
+                    avatar: 'img/avatar_8.jpg',
                     visible: true,
                     messages: [
                         {
